6fdde05049909e80755d31b832770f2e2b790442
mansha <manshapandita@gmail.com>
2021-08-08 11:52:48 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import {Switch, Route} from 'react-router-dom'
+// here we have used exact which is said to be true as it does't require the equals to [exact={true}] statement in react:
 function App() {
-  return <div >h1</div>;
+  return (
+  <Switch>
+    <Route exact path="/"> 
+      This is home page
+      </Route>
+
+      <Route exact path="/starred"> 
+      This is starred
+      </Route>
+
+      <Route>
+        This is 404 page
+      </Route>
+
+  </Switch>
+  );
 }
 
 export default App;
