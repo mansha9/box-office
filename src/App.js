33e0ecaae67ec2b837182c3ad7c297a5c8d6274b
mansha <manshapandita@gmail.com>
2021-08-08 12:42:32 +0530

    created navbar and created page components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from "react";
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+
 // here we have used exact which is said to be true as it does't require the equals to [exact={true}] statement in react:
 function App() {
   return (
+    <div>
+
+      <Navs />
   <Switch>
     <Route exact path="/"> 
-      This is home page
-      </Route>
+     <Home /> 
+    </Route>
 
       <Route exact path="/starred"> 
-      This is starred
+      <Starred /> 
       </Route>
 
       <Route>
-        This is 404 page
+        <div>
+          Not found
+        </div>
       </Route>
 
   </Switch>
+  </div>
   );
 }
 
